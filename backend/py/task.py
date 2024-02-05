7c8e7226a1367e583d1aada84cf315bdf2551965
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-02-05 20:58:16 +1100

    finished backend task

--- backend/py/task.py
+++ backend/py/task.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass
 
 @dataclass
@@ -13,21 +14,68 @@ class File:
 Task 1
 """
 def leafFiles(files: list[File]) -> list[str]:
-    return []
+    parentset = set()
+
+    for f in files:
+        if f.parent != -1:
+            parentset.add(f.parent)
+
+    return [fl.name for fl in files if fl.id not in parentset]
 
 
 """
 Task 2
 """
 def kLargestCategories(files: list[File], k: int) -> list[str]:
-    return []
+    categorycount = {}
 
+    freq = [[]]
+
+    for f in files:
+        for c in f.categories:
+            categorycount[c] = categorycount.get(c, 0) + 1
+        freq.append([])
+
+    for name, category in categorycount.items():
+        freq[category].append(name)
+
+    final_res = []
+
+    for x in freq[::-1]:
+        for category in sorted(x):
+            final_res.append(category)
+            if len(final_res) == k:
+                return final_res
 
 """
 Task 3
 """
 def largestFileSize(files: list[File]) -> int:
-    return 0
+    adjMap = {}
+    totalSizeMap = {}
+
+    for f in files:
+        adjMap[f.id] = []
+
+    for f in files:
+        if f.parent != -1:
+            adjMap[f.parent].append(f)
+
+    maxSize = 0
+
+    def dfs(f: File, adjMap: dict, totalSizeMap: dict):
+        if f.id in totalSizeMap:
+            return totalSizeMap[f.id]
+        sum = f.size
+        for child in adjMap[f.id]:
+            sum += dfs(child, adjMap, totalSizeMap)
+        totalSizeMap[f.id] = sum
+        return sum
+
+    for f in files:
+        maxSize = max(maxSize, dfs(f, adjMap, totalSizeMap))
+
+    return maxSize
 
 
 if __name__ == '__main__':
