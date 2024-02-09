f732051ece5bcf8d222769a4a78ba921c3ca7c82
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-02-09 15:52:42 +1100

    added comments

--- backend/py/task.py
+++ backend/py/task.py
@@ -14,12 +14,18 @@ class File:
 Task 1
 """
 def leafFiles(files: list[File]) -> list[str]:
+    # intialise a set for all the parents
+    # O(n)
     parentSet = set()
 
+    # loop through all the files, and if we find
+    # a file with a parent that isn't -1, 
+    # add it.
     for f in files:
         if f.parent != -1:
             parentSet.add(f.parent)
 
+    # now we just return a list of all files that are not in the parent set
     return [fl.name for fl in files if fl.id not in parentSet]
 
 
@@ -27,54 +33,87 @@ def leafFiles(files: list[File]) -> list[str]:
 Task 2
 """
 def kLargestCategories(files: list[File], k: int) -> list[str]:
+    # counts the number of of each category in the list
     categoryCount = {}
 
+    # an array used to store, in order, the frequencies of the categories into buckets.
+    # i.e. if a category 'documents' appeared twice in the files list, then it is stored
+    # in the array at index 2.
     freq = [[]]
 
+    # counts, the categories and initialises the frequency array counter. (max frequency
+    # of anything is bounded by the number of files)
     for f in files:
         for c in f.categories:
             categoryCount[c] = categoryCount.get(c, 0) + 1
         freq.append([])
 
+    # adds the categories and groups them by their frequencies
     for name, category in categoryCount.items():
         freq[category].append(name)
 
     final_res = []
 
+    # loop backwards, since we wwant the top k most frequent
     for x in freq[::-1]:
+        # sort each category alphabetically, to satisfy the second requirement
         for category in sorted(x):
             final_res.append(category)
             if len(final_res) == k:
                 return final_res
+    # O(n + k log k)
 
 """
 Task 3
 """
 def largestFileSize(files: list[File]) -> int:
+    # this problem is analogous to a graph problem.
+    # we are trying to find all possible children and grandchildren, and get a size there.
+    # it stands to reason that we connect these with an adjacencyMap,
+    # then we can run DFS on that graph, and check for maxSize.
+
+    # map to track adjacencies in the graph.
+    # we can describe adjacencies in a one way relationship, where A is
+    # adjacent to B if A is a parent of B.
     adjMap = {}
+
+    # this map keeps track of elements that we've already calculated
+    # the totalSize for, to reduce any repeated work.
     totalSizeMap = {}
 
+    # Initialise the adjacency Map.
     for f in files:
         adjMap[f.id] = []
 
+    # for every file, look up the parent,
+    # then append itself to its parent's array.
     for f in files:
         if f.parent != -1:
             adjMap[f.parent].append(f)
 
     maxSize = 0
 
+    # DFS to traverse through the whole map we created, and to update the maxSize accordingly.
     def dfs(f: File, adjMap: dict, totalSizeMap: dict):
+        # if the id is already visited, return it.
         if f.id in totalSizeMap:
             return totalSizeMap[f.id]
+        # we need to calculate total size, so we 
+        # must visit all neighbours and add their total sizes
         sum = f.size
         for child in adjMap[f.id]:
             sum += dfs(child, adjMap, totalSizeMap)
+
+        # once found, update the totalSizeMap.
         totalSizeMap[f.id] = sum
         return sum
 
+    # for the final step, to ensure we visit all files, (including the ones)
+    # that have no parent or child relationship, we must loop through them 
+    # and run the dfs function.
     for f in files:
         maxSize = max(maxSize, dfs(f, adjMap, totalSizeMap))
-
+        
     return maxSize
 
 
