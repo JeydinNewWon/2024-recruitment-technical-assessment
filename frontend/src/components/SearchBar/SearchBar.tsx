2275a5cd0df4712e94fdbd12a6d9008fab394820
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-02-10 03:04:23 +1100

    okay actually done now

--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -16,14 +16,14 @@ export function SearchBar() {
     return (
       <>
         <div className="flex flex-col justify-center w-full items-center">
-          <div className="flex w-5/6 items-center bg-white rounded border-2 border-unilectives-search" onClick={update}>
+          <div className="flex w-5/6 items-center bg-white rounded border-2" onClick={update}>
             <HiMiniMagnifyingGlass className="w-6 h-6 text-unilectives-search mx-2" style={{color: 'rgb(156 173 233)'}}/>
-            <input type="text" className="cursor-pointer bg-transparent w-full py-2 px-3 text-sm focus:outline-none placeholder-unilectives-search font-medium" autoComplete="off" placeholder="Search for a course e.g. COMP1511" ></input>
+            <input type="text" className="cursor-pointer bg-transparent w-full py-2 px-3 text-sm focus:outline-none placeholder-unilectives-search font-medium" autoComplete="off" placeholder="Search for a course e.g. COMP1511" style={{color: 'rgb(150, 150, 150)'}}></input>
           </div>
           <div className="fixed text-black z-[999] px-4 py-2 bg-white rounded-md border-black border-solid border-2 w-[300px]" style={{display: display, left: 'calc(50vw - 115px)', top: 'calc(50vh - 120px)' }}>
               <p>Hi, all! Please stan Archbishop Rhea!</p>
               <img src='./src/assets/rhea.png'></img>
-              <button className="hover:bg-[#a10ef1] border-0" onClick={update}>Close</button>
+              <button className="shadow-md bg-[#42da9f] text-white border-0" onClick={update}>Close</button>
           </div>
         </div>
       </>
