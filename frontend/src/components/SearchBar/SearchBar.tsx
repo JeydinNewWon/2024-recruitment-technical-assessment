8dd7c7f5f0b854969417d5e6f108b71938f55a78
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-02-10 03:11:53 +1100

    wow done

--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -4,26 +4,17 @@ import { HiMiniMagnifyingGlass } from "react-icons/hi2";
 export function SearchBar() {
     let [display, setDisplay] = useState('none');
 
-    function update() {
-      if (display === 'none') {
-        setDisplay('block');
-      } else {
-        setDisplay('none');
-      }
-      console.log(display);
-    }
-
     return (
       <>
         <div className="flex flex-col justify-center w-full items-center">
-          <div className="flex w-5/6 items-center bg-white rounded border-2" onClick={update}>
-            <HiMiniMagnifyingGlass className="w-6 h-6 text-unilectives-search mx-2" style={{color: 'rgb(156 173 233)'}}/>
-            <input type="text" className="cursor-pointer bg-transparent w-full py-2 px-3 text-sm focus:outline-none placeholder-unilectives-search font-medium" autoComplete="off" placeholder="Search for a course e.g. COMP1511" style={{color: 'rgb(150, 150, 150)'}}></input>
+          <div className="flex w-5/6 items-center bg-white rounded border-2 border-[#9cade9]" onClick={() => setDisplay('block')}>
+            <HiMiniMagnifyingGlass className="w-6 h-6 text-unilectives-search mx-2" style={{color: '#9cade9'}}/>
+            <input id="searchbar" type="text" className="cursor-pointer bg-transparent w-full py-2 px-3 text-sm focus:outline-none placeholder-unilectives-search font-medium" autoComplete="off" placeholder="Search for a course e.g. COMP1511" style={{color: 'rgb(156 173 233)'}}></input>
           </div>
           <div className="fixed text-black z-[999] px-4 py-2 bg-white rounded-md border-black border-solid border-2 w-[300px]" style={{display: display, left: 'calc(50vw - 115px)', top: 'calc(50vh - 120px)' }}>
               <p>Hi, all! Please stan Archbishop Rhea!</p>
               <img src='./src/assets/rhea.png'></img>
-              <button className="shadow-md bg-[#42da9f] text-white border-0" onClick={update}>Close</button>
+              <button className="shadow-md bg-[#42da9f] text-white border-0" onClick={() => setDisplay('none')}>Close</button>
           </div>
         </div>
       </>
