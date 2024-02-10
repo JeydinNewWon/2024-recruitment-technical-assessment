3b68a52a6ff7c764d478372cb6f46b5830231f8e
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-02-10 19:32:43 +1100

    oops added arrow down

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -1,9 +1,12 @@
+import { HiChevronDown} from 'react-icons/hi2';
+
 export function Dropdown() {
     return (
         <div className='w-5/6 justify-center items-center'>
             <div className="flex-1 mt-4 min-w-[150px] max-w-[200px]">
-                <button className="relative flex items-center justify-between gap-2 w-full text-black cursor-pointer bg-white py-2 px-4 text-left font-normal dropdown-border shadow-md">
-                Alphabetical (A-Z)
+                <button className="relative flex items-center justify-between gap-2 w-full text-black cursor-pointer bg-white py-2 px-4 text-left font-normal dropdown-border shadow-md" style={{color: 'rgb(153, 153, 153)'}}>
+                    Sort By 
+                    <HiChevronDown style={{color: 'black'}}/>
                 </button>
             </div>
         </div>
